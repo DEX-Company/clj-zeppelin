929f83817e656d02b2091b5afbe3bd4bfa0b4503
kavyapc01 <kavya.premchandran@uvionicstech.com>
2018-10-12 11:32:58 +0530

    updates to add a test to create paragraph

--- test/clj_zeppelin/core_test.clj
+++ test/clj_zeppelin/core_test.clj
@@ -41,10 +41,10 @@
 
 (defn create-note-helper
   [nbserver1]
-  (let [note-id (create-note! nbserver1  {:name "clojure note1"})
+  (let [note-id (create-note! nbserver1  {:name "clojure note12"})
            ids (map :id (get-in (list-notes nbserver1) [:body]))]
      {:created-note-id note-id :retrieved-note-ids ids})) 
-  
+
  
 (deftest test-create-note
   (let [note-id (create-note-helper nbserver1)
@@ -57,34 +57,58 @@
    note-id (:created-note-id ret-ids)
    return-ids (:retrieved-note-ids ret-ids)
    x (some #{note-id} return-ids)]
-       (if (not (= nil x))
+       (if x
           (let [sts (delete-note! nbserver1 note-id)             
             y (some #{note-id} (map :id (get-in (list-notes nbserver1) [:body])))]
-            (is (nil? y))))))
+            (is (nil? y)))
+       (println "note-id not present in the returned list of ids"))))
 
 
-;;test for create para
+;;test for create paragraph
 (deftest test-create-para
   (let [ret-ids (create-note-helper nbserver1)
    note-id (:created-note-id ret-ids)
    return-ids (:retrieved-note-ids ret-ids)
    x (some #{note-id} return-ids)]
-       (if (not (nil? x))
+       (if x
          (let [para-id (create-paragraph! nbserver1 note-id (-> {:title "intro"
                                                                  :text "\n Hello user"}))]          
-           (if (not (nil? para-id))
+           (if para-id
              (let [sts (get-paragraph-status nbserver1 note-id para-id)]
                 (if (= "READY" (:status sts))
                  (let [res (:text (get-paragraph-info nbserver1 note-id para-id))]
-                  (is (= "\n Hello user"  res))))))))))
-             
-                    
-      
+                  (is (= "\n Hello user"  res)))
+                (println "incorrect paragraph status")))
+            (println "paragraph id not returned")))
+         (println "note-id not present in the returned list of ids"))))
 
 
+;;delete-note used in fixture function
+(defn delete-note
+  [nbserbver1]
+  (let [list-id (list-notes nbserver1)
+     ids (map :id (get-in (list-notes nbserver1) [:body]))
+     names (map :name (get-in (list-notes nbserver1) [:body]))
+     idx (map first 
+              (filter #(= (second %) "clojure note12")
+                      (map-indexed vector names)))
+     cnt (count idx)]
+       (loop [ct cnt indx 0]
+         (if (> ct 0)
+           (let [x (some #{(nth ids (nth idx indx))} ids)]
+             (delete-note! nbserver1 x)
+             (recur (dec ct) (inc indx)))
+           ))))
+              
 
+(defn create-delete-fixture
+[f]
+(create-note-helper nbserver1)
+(f)
+(delete-note nbserver1)
+)
 
+(clojure.test/use-fixtures :once create-delete-fixture)
 
-
-
-
+;to run all the tests
+;(clojure.test/run-tests)
