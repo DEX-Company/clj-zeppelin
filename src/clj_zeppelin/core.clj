57e2c5a29ced5a539474dd94cf9c1fa431538ed4
shark8me <kiran.karkera@gmail.com>
2019-01-16 23:39:42 +0800

    remove API path hardcoding, allow tests to get host path from docker

--- src/clj_zeppelin/core.clj
+++ src/clj_zeppelin/core.clj
@@ -7,7 +7,12 @@
 (defn- kwdize-resp
   "keywordize the response when successful"
   [resp]
-  (-> resp :body json/read-str clojure.walk/keywordize-keys))
+  (let [istr (-> resp :body )]
+    (try
+      (do 
+        (clojure.walk/keywordize-keys (json/read-str istr)))
+      (catch Exception e
+        (println " kwdize error " istr)))))
 
 (defn json-req
   "create a json string from a clojure map"
@@ -20,19 +25,14 @@
   "
   List the existing notes
   https://zeppelin.apache.org/docs/0.8.0/usage/rest_api/notebook.html#list-of-the-notes
-
   "
-  [notebook-server-url]
- 
-  (let [resp @(ht/request {:url (str notebook-server-url "/api/notebook")
+  [notebook-api-url]
+  (let [resp @(ht/request {:url notebook-api-url
                            :method :get})]
     (if (:error resp)
       (throw (ex-info " error listing notes " resp))
       (kwdize-resp resp))))
 
-;;(list-notes "http://localhost:8081")
- 
- (def nbserver "http://192.168.0.182:8080")
 (defn create-note!
   "
   create a new note
@@ -40,21 +40,13 @@
 
   Returns the id of the new note
   "
-  [notebook-server-url payload]
-  (let [resp @(ht/request {:url (str notebook-server-url "/api/notebook")
+  [notebook-api-url payload]
+  (let [resp @(ht/request {:url notebook-api-url
                            :method :post
                            :body (finalize payload)})]
     (if (:error resp)
       (throw (ex-info " error creating note notes " resp))
       (-> resp :body json/read-str (get "body")))))
-
-
-#_(def note-id (create-note! nbserver
-                             (-> {:name "Ocean Notebook"
-                                  :paragraphs [{:title "intro"
-                                                :text "%md\n Hello world\n"}]}
-                                 stringify-keys
-                                 json/write-str)))
 
 (defn delete-note!
   "
@@ -62,14 +54,12 @@
   https://zeppelin.apache.org/docs/0.8.0/usage/rest_api/notebook.html#delete-a-note
   Returns the status 
   "
-  [notebook-server-url note-id]
-  (let [resp @(ht/request {:url (str notebook-server-url "/api/notebook/" note-id )
+  [notebook-api-url note-id]
+  (let [resp @(ht/request {:url (str notebook-api-url note-id )
                            :method :delete})]
     (if (:error resp)
       (throw (ex-info " error deleting note " note-id ", response:  " resp))
       (kwdize-resp resp))))
-
-;;(delete-note! nbserver "2DRF4MVSW")
 
 (defn import-note!
   "
@@ -87,14 +77,12 @@
   https://zeppelin.apache.org/docs/0.8.0/usage/rest_api/notebook.html#run-all-paragraphs
   Returns status as a map
   "
-  [notebook-server-url note-id]
-  (let [resp @(ht/request {:url (str notebook-server-url "/api/notebook/job/" note-id)
+  [notebook-api-url note-id]
+  (let [resp @(ht/request {:url (str notebook-api-url "job/" note-id)
                            :method :post})]
     (if (:error resp)
       (throw (ex-info " error running note " resp))
       (kwdize-resp resp))))
-
-;;(run-all-paragraphs nbserver "2DTJSPSWV")
 
 (defn create-paragraph!
   "Creates a new paragraph, added to note-id.
@@ -102,30 +90,17 @@
 
   returns paragraph id
   "
-  [notebook-server-url note-id paragraph-data]
-  (let [resp @(ht/request {:url (str notebook-server-url "/api/notebook/" note-id "/paragraph")
+  [notebook-api-url note-id paragraph-data]
+  (let [resp @(ht/request {:url (str notebook-api-url note-id "/paragraph")
                            :method :post
                            :body (finalize paragraph-data)})]
     (if (:error resp)
       (throw (ex-info " error creating paragraph " resp))
       (-> resp kwdize-resp :body))))
 
-#_(create-paragraph! nbserver
-                   "2DTMA8RR9"
-                   (-> {:title "intro"
-                        :text "%md\n Hello paragraph \n"}
-                       json-req))
-;;create first paragraph instead of appending to the last one 
-#_(create-paragraph! nbserver
-                   "2DTMA8RR9"
-                   (-> {:title "intro"
-                        :text "%md\n Hello first paragraph \n"
-                        :index 0}
-                       json-req))
-
 (defn get-paragraph-status
-  [notebook-server-url note-id paragraph-id]
-  (let [resp @(ht/request {:url (str notebook-server-url "/api/notebook/job/" note-id "/" paragraph-id)
+  [notebook-api-url note-id paragraph-id]
+  (let [resp @(ht/request {:url (str notebook-api-url "job/" note-id "/" paragraph-id)
                            :method :get})]
     (if (:error resp)
       (throw (ex-info " error getting status " resp))
@@ -137,22 +112,12 @@
   https://zeppelin.apache.org/docs/0.8.0/usage/rest_api/notebook.html#get-a-paragraph-information
 
   Takes as arguments the notebook server, the note id and paragraph id"
-  [notebook-server-url note-id paragraph-id]
-  (let [resp @(ht/request {:url (str notebook-server-url "/api/notebook/" note-id "/paragraph/" paragraph-id)
+  [notebook-api-url note-id paragraph-id]
+  (let [resp @(ht/request {:url (str notebook-api-url  note-id "/paragraph/" paragraph-id)
                            :method :get})]
     (if (:error resp)
       (throw (ex-info " error getting paragraph info " resp))
       (-> resp kwdize-resp :body))))
-
-#_(def res (let [note-id (create-note! nbserver (note "new2"
-                                                    [(paragraph "title" :md ["hello new para"])]))
-               para-id (create-paragraph! nbserver note-id 
-                                          (paragraph "new1" :sh ["mkdir output2"]))
-               res (run-all-paragraphs nbserver note-id)
-               ]
-           (get-paragraph-info nbserver note-id para-id)))
-
-;(-> res )
 
 (defn run-paragraph-async
   "runs a paragraph asynchronously
@@ -161,13 +126,12 @@
   Takes as arguments the notebook server, the note id and paragraph id.
 
   Returns immediately, and the status is usually PENDING"
-  [notebook-server-url note-id paragraph-id]
-  (let [resp @(ht/request {:url (str notebook-server-url "/api/notebook/job/" note-id "/" paragraph-id)
+  [notebook-api-url note-id paragraph-id]
+  (let [resp @(ht/request {:url (str notebook-api-url "job/" note-id "/" paragraph-id)
                            :method :post})]
     
     (if (:error resp)
       (throw (ex-info " error running paragraph " resp))
-;      (-> resp kwdize-resp :body)
       (-> resp kwdize-resp :status))))
 
 (defn run-paragraph-sync
@@ -177,19 +141,9 @@
   Takes as arguments the notebook server, the note id and paragraph id.
 
   Returns after the paragraph completes execution "
-  [notebook-server-url note-id paragraph-id]
-  (let [resp @(ht/request {:url (str notebook-server-url "/api/notebook/run/" note-id "/" paragraph-id)
+  [notebook-api-url note-id paragraph-id]
+  (let [resp @(ht/request {:url (str notebook-api-url "run/" note-id "/" paragraph-id)
                            :method :post})]
     (if (:error resp)
       (throw (ex-info " error running paragraph " resp))
       (-> resp kwdize-resp :body))))
-
-#_(let [note-id (create-note! nbserver (note "new5"
-                                           [(paragraph "title" :md ["hello new para"])]))
-      para-id (create-paragraph! nbserver note-id 
-                                 (paragraph "new1" :sh ["mkdir output2"]))
-      res (run-paragraph-sync nbserver note-id para-id)
-      ]
-  (get-paragraph-info nbserver note-id para-id))
-
-
--- test/clj_zeppelin/core_test.clj
+++ test/clj_zeppelin/core_test.clj
@@ -7,12 +7,13 @@
 
  ;;prove interaction with fixture by init-fn
 (def fixture-response (atom nil))
+(def api-url (atom ""))
 
 ;; easy http GET
 (defn component-http-get
   ([host]
    (let [resp @(http/get (str "http://localhost:8080/api/notebook/"))]
-     (println " calling get, resp is " (:body resp) " host " host)
+     (reset! api-url (-> resp :opts :url))
      resp)))
 
 (use-fixtures :once
@@ -25,59 +26,54 @@
  ;;did the init-fn interact with the fixture?
 (deftest test-fixture-init
   (let [resp (:status @fixture-response)]
-    (println " got response " resp)
     (is (= 200 resp))))
 
 
 (deftest test-list-notes
-  (let [resp (list-notes "http://localhost:8080")
+  (let [resp (list-notes @api-url)
         num-notes (-> resp :body count)]
     (println " list notes " num-notes)
     (is (> num-notes 0))))
 
-
-(def nbserver1 "http://localhost:8080")
-
-
 (defn create-note-helper
-  [nbserver1]
-  (let [note-id (create-note! nbserver1  {:name "clojure note12"})
-       ids (map :id (get-in (list-notes nbserver1) [:body]))]
+  [zepl]
+  (let [note-id (create-note! zepl {:name "clojure note12"})
+       ids (map :id (get-in (list-notes zepl) [:body]))]
    {:created-note-id note-id :retrieved-note-ids ids})) 
 
 
 ;;delete-note used in fixture function
 (defn delete-note
-  []
-(let [list-id (list-notes nbserver1) 
-     ids-to-delete  (->> (get-in list-id [:body])
-                                    ;;filter those that have :name = clojure note 12
-                                  (filter #(= (:name %) "clojure note12"))
-                                    ;;get the ids of those that pass the filter.
-                                   (map :id))]
- (doseq  [ids1 ids-to-delete] (delete-note! nbserver1 ids1))))
+  [zepl]
+  (let [list-id (list-notes zepl)
+        ids-to-delete  (->> (get-in list-id [:body])
+                            ;;filter those that have :name = clojure note 12
+                            (filter #(= (:name %) "clojure note12"))
+                            ;;get the ids of those that pass the filter.
+                            (map :id))]
+    (doseq  [ids1 ids-to-delete] (delete-note! zepl ids1))))
 
 
 ;;test-create-note
 (deftest test-create-note
-  (let [note-id (create-note-helper nbserver1)
+  (let [note-id (create-note-helper @api-url)
    x (some #{(:created-note-id note-id)} (:retrieved-note-ids note-id))]
       (testing "note-id present in the returned list of ids or absent"
         (is x)
         ))
-  (delete-note))
+  (delete-note @api-url))
 
 
 ;;test-delete-note
 (deftest test-delete-note
-  (let [ret-ids (create-note-helper nbserver1)
+  (let [ret-ids (create-note-helper @api-url)
    note-id (:created-note-id ret-ids)
    return-ids (:retrieved-note-ids ret-ids)
    x (some #{note-id} return-ids)]
     (testing "note-id present in the returned list of ids or absent"
        (is x))
-          (let [sts (delete-note! nbserver1 note-id)             
-            y (some #{note-id} (map :id (get-in (list-notes nbserver1) [:body])))]
+          (let [sts (delete-note! @api-url note-id)             
+            y (some #{note-id} (map :id (get-in (list-notes @api-url) [:body])))]
               (testing "note-id deleted or not deleted"
                 (is (nil? y)))
        )))
@@ -86,15 +82,15 @@
 ;;create paragraph helper
 (defn create-para-helper
   [note-id]
-  (let [para-id (create-paragraph! nbserver1 note-id (-> {:title "intro"
+  (let [para-id (create-paragraph! @api-url note-id (-> {:title "intro"
                                                           :text  (+ 10 10)}))
-        para-status (get-paragraph-status nbserver1 note-id para-id)]
+        para-status (get-paragraph-status @api-url note-id para-id)]
     {:paragraph-id para-id :paragraph-status para-status}))
 
 
 ;;test-create-paragraph
 (deftest test-create-para
-  (let [ret-ids (create-note-helper nbserver1)
+  (let [ret-ids (create-note-helper @api-url)
    note-id (:created-note-id ret-ids)
    return-ids (:retrieved-note-ids ret-ids)
    x (some #{note-id} return-ids)]
@@ -107,16 +103,16 @@
           (is para-id))
         (testing "paragraph status check"
             (is (= "READY" (:status para-status))))
-        (let [res (:text (get-paragraph-info nbserver1 note-id para-id))]
+        (let [res (:text (get-paragraph-info @api-url note-id para-id))]
             (testing "paragraph content check"
               (is (= "20"  res))
               ))))
-  (delete-note))
+  (delete-note @api-url))
      
   
 ;;test-run-paragraph-asynchronously
 (deftest test-para-async
-  (let [ret-ids (create-note-helper nbserver1)
+  (let [ret-ids (create-note-helper @api-url)
    note-id (:created-note-id ret-ids)
    return-ids (:retrieved-note-ids ret-ids)
    x (some #{note-id} return-ids)]
@@ -129,17 +125,17 @@
           (is para-id))
         (testing "paragraph status check"
             (is (= "READY" (:status para-status))))
-            (let [async-sts (run-paragraph-async nbserver1 note-id para-id)]
-             (println " async status " (get-paragraph-status nbserver1 note-id para-id))
+            (let [async-sts (run-paragraph-async @api-url note-id para-id)]
+             (println " async status " (get-paragraph-status @api-url note-id para-id))
               (testing "check status of run paragraph asynchronously"
                  (is (= "OK" async-sts)))
               )))
-  (delete-note))
+  (delete-note @api-url))
  
 
 (defn create-para-helper-sync
   [note-id]
-  (let [para-id (create-paragraph! nbserver1 note-id (-> {:title "intro"
+  (let [para-id (create-paragraph! @api-url note-id (-> {:title "intro"
                                                           :text  "%python
 def nth_prime_number(n):
     # initial prime number list
@@ -170,12 +166,12 @@ def nth_prime_number(n):
     return prime_list[-1]
 nth_prime_number(100)
 "}))
-        para-status (get-paragraph-status nbserver1 note-id para-id)]
+        para-status (get-paragraph-status @api-url note-id para-id)]
     {:paragraph-id para-id :paragraph-status para-status}))
 
 
 (deftest status-run-para-sync
-  (let [ret-ids (create-note-helper nbserver1)
+  (let [ret-ids (create-note-helper @api-url)
    note-id (:created-note-id ret-ids)
    return-ids (:retrieved-note-ids ret-ids)
    x (some #{note-id} return-ids)]
@@ -190,15 +186,15 @@ nth_prime_number(100)
           (is (= "READY" (:status para-status))))
          
 ;         test status run-para-sync
-          (let [f (future (run-paragraph-sync nbserver1 note-id para-id))]
+          (let [f (future (run-paragraph-sync @api-url note-id para-id))]
            (doseq [i (range 20)]
             (do (Thread/sleep 1000)
-            (println " ith " i " iteration, result " (get-paragraph-status nbserver1 note-id para-id))))
+            (println " ith " i " iteration, result " (get-paragraph-status @api-url note-id para-id))))
            (println " future result completed" @f)
            (testing "check output status of run paragraph synchronously"
            (is (= 541 (Integer/parseInt (.replace (-> @f :msg first :data) "\n" "")))))
-           (println " last " " iteration, result " (get-paragraph-status nbserver1 note-id para-id)))))
-           (delete-note))
+           (println " last " " iteration, result " (get-paragraph-status @api-url note-id para-id)))))
+           (delete-note @api-url))
   
 
 
